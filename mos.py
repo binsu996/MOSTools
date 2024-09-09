b2b2c5e8d628ace3ee53ec808b151281456fb438
subin <binsu.cs@gmail.com>
2024-09-09 07:12:40 +0000

    update show format

--- mos.py
+++ mos.py
@@ -4,6 +4,7 @@ import pandas as pd
 import fire
 import numpy as np
 from functools import partial
+from omegaconf import OmegaConf
 
 
 def read_url_table(filename):
@@ -11,7 +12,8 @@ def read_url_table(filename):
     return df
 
 
-def gen_one_page(df, metrics, save_to_dir, page_id=0, first_is_gt=False, shuffle=True):
+def gen_one_page(df, metrics, save_to_dir, page_id=0, first_is_gt=False, shuffle=True, show_name=False):
+    np.random.seed(666)
     data = df.values
     ngroup = len(df.columns)
     n_metrics = len(metrics)
@@ -34,7 +36,7 @@ def gen_one_page(df, metrics, save_to_dir, page_id=0, first_is_gt=False, shuffle
             for j, (col, audio_with_name) in enumerate(zip(cols, audio_with_names)):
                 audio, name = audio_with_name
                 with col:
-                    if not shuffle:
+                    if not shuffle or show_name:
                         st.write(name)
                     st.audio(
                         audio, format="audio/{}".format(audio.split(".")[-1]))
@@ -81,22 +83,23 @@ def login_page():
         return username
 
 
-def render(filename, metrics, first_is_gt, save_to_dir, shuffle):
+def render(filename, metrics, first_is_gt, save_to_dir, shuffle, show_name=True):
     st.set_page_config(page_title="MOS 评测", layout="wide")
     df = read_url_table(filename)
-    n = np.ceil(len(df)/10)
+    n = np.ceil(len(df)/5)
     dfs = np.array_split(df, n)
 
     options = st.sidebar.radio("测评任务子集", range(len(dfs)))
-    pages = [partial(gen_one_page, sub_df, metrics, save_to_dir, idx, first_is_gt, shuffle)
+    pages = [partial(gen_one_page, sub_df, metrics, save_to_dir, idx, first_is_gt, shuffle, show_name)
              for idx, sub_df in enumerate(dfs)]
     pages[options]()
 
 
-def run_mos(filename, save_to_dir="tmp", shuffle=True, first_is_gt=False):
+def run_mos(filename, save_to_dir="tmp", shuffle=True, first_is_gt=False, config="config/default.yaml", show_name=False):
     Path(save_to_dir).mkdir(exist_ok=True, parents=True)
-    render(filename, metrics=["PhonemeClarity", "ReverberationSeverity", "OtherNoiseLevel", "Overall"],
-           first_is_gt=first_is_gt, save_to_dir=save_to_dir, shuffle=shuffle)
+    config = OmegaConf.load(config)
+    render(filename, metrics=config.metrics,
+           first_is_gt=first_is_gt, save_to_dir=save_to_dir, shuffle=shuffle, show_name=show_name)
 
 
 if __name__ == "__main__":
--- show_mos.py
+++ show_mos.py
@@ -4,9 +4,16 @@ import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import ttest_rel
+from itertools import product
+from pathlib import Path
+import numpy as np
+
 sns.set_theme(style="ticks")
 
-df = sns.load_dataset("penguins")
+
+def highlight(x, p_value):
+    return np.where(p_value is not None and p_value < 0.05, f"background-color: rgb(215,96,146,0.3) ", None)
 
 
 def main(res_folder):
@@ -14,14 +21,40 @@ def main(res_folder):
            for x in Path(res_folder).glob("*.xlsx")]
     df = pd.concat(dfs)
 
-    for metric,sub_df in df.groupby("metric"):
+    for metric, sub_df in df.groupby("metric"):
         st.header(metric)
-        st.dataframe(sub_df[["audio","score"]])
-        fig=plt.figure()
-        hue_order=sorted(set(sub_df["name"].values))
-        sns.countplot(sub_df, x="score",hue="name",hue_order=hue_order)
+        # st.dataframe(sub_df)
+        fig = plt.figure()
+        hue_order = sorted(set(sub_df["name"].values))
+        sns.countplot(sub_df, x="score", hue="name", hue_order=hue_order)
         plt.title(metric)
         st.pyplot(fig)
+
+        sub_df["baseaudio"] = list(
+            map(lambda x: Path(x).stem, sub_df["audio"]))
+        wide_sub_df = pd.pivot(
+            sub_df, index=["baseaudio", "username"], columns='name', values='score').reset_index()
+
+        st.dataframe(wide_sub_df.describe(
+        ).loc["mean"], use_container_width=True)
+
+        # 计算均值差异，以及pvalue，将结果绘制在表格中
+        diff_df = pd.DataFrame(index=hue_order, columns=hue_order)
+        pvalue_df = pd.DataFrame(index=hue_order, columns=hue_order)
+
+        n = len(hue_order)
+        for i, j in product(range(n), repeat=2):
+            if i > j:
+                name_i = hue_order[i]
+                name_j = hue_order[j]
+                sj = wide_sub_df[name_j].values
+                si = wide_sub_df[name_i].values
+                diff_df[name_i][name_j] = sj.mean()-si.mean()
+                _, pvalue_df[name_i][name_j] = ttest_rel(sj, si)
+
+        st.dataframe(diff_df.style.apply(
+            highlight, p_value=pvalue_df.values, axis=None), use_container_width=True)
+
 
 if __name__ == "__main__":
     fire.Fire(main)
